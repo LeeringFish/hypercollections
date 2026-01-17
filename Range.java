45b556ff2113a69f609c16a5776dd34c885b2259
LeeringFish <theleeringfish@proton.me>
2026-01-17 18:12:23 -0500

    add rest of factory methods to Range.java

--- Range.java
+++ Range.java
@@ -1,5 +1,7 @@
 package collections;
 
+import java.util.Objects;
+
 public class Range<T extends Comparable<? super T>> {
     private final T elementA;
     private final T elementB;
@@ -14,27 +16,61 @@ public class Range<T extends Comparable<? super T>> {
     }
 
     public static <T extends Comparable<? super T>> Range<T> open(T a, T b) {
-        if (a == null || b == null) {
-            throw new NullPointerException();
-        }
-
-        if (a.compareTo(b) >= 0) {
-            throw new IllegalArgumentException();
-        }
-
-        return new Range<>(a, b, false, false);
+        return between(a, b, false, false);
     }
 
     public static <T extends Comparable<? super T>> Range<T> closed(T a, T b) {
-        if (a == null || b == null) {
-            throw new NullPointerException();
-        }
+        return between(a, b, true, true);
+    }
 
-        if (a.compareTo(b) > 0) {
+    public static <T extends Comparable<? super T>> Range<T> openClosed(T a, T b) {
+        return between(a, b, false, true);
+    }
+
+    public static <T extends Comparable<? super T>> Range<T> closedOpen(T a, T b) {
+        return between(a, b, true, false);
+    }
+
+    public static <T extends Comparable<? super T>> Range<T> greaterThan(T a) {
+        return lower(a, false);
+    }
+
+    public static <T extends Comparable<? super T>> Range<T> atLeast(T a) {
+        return lower(a,true);
+    }
+
+    public static <T extends Comparable<? super T>> Range<T> lessThan(T b) {
+        return upper(b,false);
+    }
+
+    public static <T extends Comparable<? super T>> Range<T> atMost(T b) {
+        return upper(b, true);
+    }
+
+    public static <T extends Comparable<? super T>> Range<T> all() {
+        return new Range<>(null, null, false, false);
+    }
+
+    private static <T extends Comparable<? super T>> Range<T> between(T a, T b, boolean leftClosed, boolean rightClosed) {
+        Objects.requireNonNull(a);
+        Objects.requireNonNull(b);
+
+        int cmp = a.compareTo(b);
+        if (cmp > 0 || (cmp == 0 && !leftClosed && !rightClosed)) {
             throw new IllegalArgumentException();
         }
 
-        return new Range<>(a, b, true, true);
+        return new Range<>(a, b, leftClosed, rightClosed);
+    }
+
+    private static <T extends Comparable<? super T>> Range<T> lower(T a, boolean inclusive) {
+        Objects.requireNonNull(a);
+        return new Range<>(a, null, inclusive, false);
+    }
+
+    private static <T extends Comparable<? super T>> Range<T> upper(T b, boolean inclusive) {
+        Objects.requireNonNull(b);
+        return new Range<>(null, b, false, inclusive);
     }
 
     public boolean contains(T value) {
